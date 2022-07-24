86ac09e3cfc910ea5251484f3588dbdf596652be
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-24 16:00:48 +0300

    delete ingredient

--- src/main/java/guru/springframework/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -16,9 +16,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class IngredientServiceImpl implements IngredientService {
 
-  private final IngredientToIngredientCommand ingredientToIngredientCommand;
   private final RecipeRepository recipeRepository;
   private final UnitOfMeasureRepository unitOfMeasureRepository;
+  private final IngredientToIngredientCommand ingredientToIngredientCommand;
   private final IngredientCommandToIngredient ingredientCommandToIngredient;
 
   public IngredientServiceImpl(
@@ -61,6 +61,7 @@ public class IngredientServiceImpl implements IngredientService {
   @Override
   @Transactional
   public IngredientCommand saveIngredientCommand(IngredientCommand command) {
+    log.debug("command01: {}", command);
     Optional<Recipe> recipeOptional = recipeRepository.findById(command.getRecipeId());
 
     if (!recipeOptional.isPresent()) {
@@ -86,17 +87,66 @@ public class IngredientServiceImpl implements IngredientService {
                 .orElseThrow(() -> new RuntimeException("UOM NOT FOUND"))); // todo address this
       } else {
         // add new Ingredient
-        recipe.addIngredient(ingredientCommandToIngredient.convert(command));
+        log.debug("command02: {}", command);
+        Ingredient ingredient = ingredientCommandToIngredient.convert(command);
+        log.debug("ingredient: {}", ingredient);
+        ingredient.setRecipe(recipe);
+        recipe.addIngredient(ingredient);
       }
 
       Recipe savedRecipe = recipeRepository.save(recipe);
 
-      // to do check for fail
-      return ingredientToIngredientCommand.convert(
+      Optional<Ingredient> savedIngredientOptional =
           savedRecipe.getIngredients().stream()
               .filter(recipeIngredients -> recipeIngredients.getId().equals(command.getId()))
-              .findFirst()
-              .get());
+              .findFirst();
+
+      // check by description
+      if (!savedIngredientOptional.isPresent()) {
+        // not totally safe... But best guess
+        savedIngredientOptional =
+            savedRecipe.getIngredients().stream()
+                .filter(
+                    recipeIngredients ->
+                        recipeIngredients.getDescription().equals(command.getDescription()))
+                .filter(
+                    recipeIngredients -> recipeIngredients.getAmount().equals(command.getAmount()))
+                .filter(
+                    recipeIngredients ->
+                        recipeIngredients.getUom().getId().equals(command.getUom().getId()))
+                .findFirst();
+      }
+
+      // to do check for fail
+      return ingredientToIngredientCommand.convert(savedIngredientOptional.get());
+    }
+  }
+
+  @Override
+  public void deleteById(Long recipeId, Long idToDelete) {
+
+    log.debug("Deleting ingredient: " + recipeId + ":" + idToDelete);
+
+    Optional<Recipe> recipeOptional = recipeRepository.findById(recipeId);
+
+    if (recipeOptional.isPresent()) {
+      Recipe recipe = recipeOptional.get();
+      log.debug("found recipe");
+
+      Optional<Ingredient> ingredientOptional =
+          recipe.getIngredients().stream()
+              .filter(ingredient -> ingredient.getId().equals(idToDelete))
+              .findFirst();
+
+      if (ingredientOptional.isPresent()) {
+        log.debug("found Ingredient");
+        Ingredient ingredientToDelete = ingredientOptional.get();
+        ingredientToDelete.setRecipe(null);
+        recipe.getIngredients().remove(ingredientOptional.get());
+        recipeRepository.save(recipe);
+      }
+    } else {
+      log.debug("Recipe Id Not found. Id:" + recipeId);
     }
   }
 }
--- src/test/java/guru/springframework/services/IngredientServiceImplTest.java
+++ src/test/java/guru/springframework/services/IngredientServiceImplTest.java
@@ -1,32 +1,34 @@
 package guru.springframework.services;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import guru.springframework.commands.IngredientCommand;
 import guru.springframework.converters.IngredientCommandToIngredient;
 import guru.springframework.converters.IngredientToIngredientCommand;
+import guru.springframework.converters.UnitOfMeasureCommandToUnitOfMeasure;
 import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import guru.springframework.domain.Ingredient;
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
+import java.util.Optional;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.util.Optional;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
-
 public class IngredientServiceImplTest {
 
   private final IngredientToIngredientCommand ingredientToIngredientCommand;
+  private final IngredientCommandToIngredient ingredientCommandToIngredient;
 
   @Mock RecipeRepository recipeRepository;
-
   @Mock UnitOfMeasureRepository unitOfMeasureRepository;
-  @Mock IngredientCommandToIngredient ingredientCommandToIngredient;
 
   IngredientService ingredientService;
 
@@ -34,6 +36,8 @@ public class IngredientServiceImplTest {
   public IngredientServiceImplTest() {
     this.ingredientToIngredientCommand =
         new IngredientToIngredientCommand(new UnitOfMeasureToUnitOfMeasureCommand());
+    this.ingredientCommandToIngredient =
+        new IngredientCommandToIngredient(new UnitOfMeasureCommandToUnitOfMeasure());
   }
 
   @Before
@@ -103,6 +107,26 @@ public class IngredientServiceImplTest {
 
     // then
     assertEquals(Long.valueOf(3L), savedCommand.getId());
+    verify(recipeRepository, times(1)).findById(anyLong());
+    verify(recipeRepository, times(1)).save(any(Recipe.class));
+  }
+
+  @Test
+  public void testDeleteById() throws Exception {
+    // given
+    Recipe recipe = new Recipe();
+    Ingredient ingredient = new Ingredient();
+    ingredient.setId(3L);
+    recipe.addIngredient(ingredient);
+    ingredient.setRecipe(recipe);
+    Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+    when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+    // when
+    ingredientService.deleteById(1L, 3L);
+
+    // then
     verify(recipeRepository, times(1)).findById(anyLong());
     verify(recipeRepository, times(1)).save(any(Recipe.class));
   }
