a445f285dc08a46e1b357fa0b7c66c3a95e0fa61
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-25 11:25:22 +0300

    customizing error messages with message source

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -137,7 +137,11 @@ public class RecipeControllerTest {
     when(recipeService.saveRecipeCommand(any())).thenReturn(command);
 
     mockMvc
-        .perform(post("/recipe").contentType(MediaType.APPLICATION_FORM_URLENCODED).param("id", ""))
+        .perform(
+            post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")
+                .param("cookTime", "3000"))
         .andExpect(status().isOk())
         .andExpect(model().attributeExists("recipe"))
         .andExpect(view().name("recipe/recipeform"));
