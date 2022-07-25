a8161005cb149732f6a81bcd6e839dc4a9f51625
Cornel Zgardan <cornel.zgardan@modex.tech>
2022-07-25 11:11:55 +0300

    data validation with spring mvc

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -3,10 +3,12 @@ package guru.springframework.controllers;
 import guru.springframework.commands.RecipeCommand;
 import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
+import javax.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -18,6 +20,8 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @Slf4j
 public class RecipeController {
+
+  private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
   private final RecipeService recipeService;
 
   public RecipeController(RecipeService recipeService) {
@@ -33,24 +37,32 @@ public class RecipeController {
   @GetMapping("/recipe/new")
   public String newRecipt(Model model) {
     model.addAttribute("recipe", new RecipeCommand());
-    return "recipe/recipeform";
+    return RECIPE_RECIPEFORM_URL;
   }
 
   @GetMapping("/recipe/{id}/update")
   public String updateRecipt(@PathVariable String id, Model model) {
     model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
-    return "recipe/recipeform";
+    return RECIPE_RECIPEFORM_URL;
   }
 
   @PostMapping("recipe")
-  public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+  public String saveOrUpdate(
+      @Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      bindingResult
+          .getAllErrors()
+          .forEach(
+              objectError -> //
+              log.error("objectError: {}", objectError));
+      return RECIPE_RECIPEFORM_URL;
+    }
     RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
     return "redirect:/recipe/" + savedCommand.getId() + "/show";
   }
 
   @GetMapping("recipe/{id}/delete")
   public String deleteById(@PathVariable String id) {
-    System.out.println();
     log.debug("Deleting id: " + id);
     recipeService.deleteById(Long.valueOf(id));
     return "redirect:/";
